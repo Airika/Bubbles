f682fabec758111e5156bec9eff9908460cc9a76
Airika <erikamz2010@hotmail.com>
2013-10-31 18:36:10 -0700

    Initial Commit

---
+++ Bubbles/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Bubbles
+//
+//  Created by Erika Martinez on 10/31/13.
+//  Copyright (c) 2013 Erika Martinez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
